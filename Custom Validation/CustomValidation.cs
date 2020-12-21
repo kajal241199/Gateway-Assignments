bd5420d18a586b7562b61fdd2096683b1d960dc2
unknown <singhvikajal258@gmail.com>
2020-12-21 22:26:03 +0530

    date modified

--- Custom Validation/CustomValidation.cs
+++ Custom Validation/CustomValidation.cs
@@ -15,7 +15,7 @@ namespace SignupWithLogin.Custom_Validation
         {
             DateTime _birthJoin = Convert.ToDateTime(value);
             DateTime minDate = Convert.ToDateTime("01-25-1970");
-            DateTime maxDate = Convert.ToDateTime("01-25-2000");
+            DateTime maxDate = Convert.ToDateTime("01-25-1998");
 
             if (_birthJoin > minDate && _birthJoin < maxDate)
                 return ValidationResult.Success;
